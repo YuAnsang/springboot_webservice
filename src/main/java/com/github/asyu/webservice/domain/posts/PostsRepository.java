4d2ae398a3989f8fdbcae2516952aeafad83ed3f
YuAnsang <kahlman@naver.com>
2020-12-20 15:15:19 +0900

    maria db client 추가 prod 설정 파일 추가

--- src/main/java/com/github/asyu/webservice/domain/posts/PostsRepository.java
+++ src/main/java/com/github/asyu/webservice/domain/posts/PostsRepository.java
@@ -2,6 +2,7 @@ package com.github.asyu.webservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
